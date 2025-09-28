df6061e33b8cec0867014acc0096ec49ecf07898
Himanshu Juyal <himanshujuyal280404@gmail.com>
2025-09-28 14:51:00 +0530

    interface - merging, extend , Interface multiple inheritance

--- 01-basics/src/tutorial4.ts
+++ 01-basics/src/tutorial4.ts
@@ -67,3 +67,67 @@ const laptop: Computer = {
 laptop.storage = 200;
 laptop.upgradeRam(10);
 console.log(laptop);
+
+interface Person {
+    name: string;
+    getDetails(): string;
+}
+
+// adding extra property
+interface Person {
+    age: number;
+}
+
+interface DogOwner {
+    dogName: string;
+    getDogDetails(): string;
+}
+
+const person: Person = {
+    name: "john",
+    age: 20,
+    getDetails() {
+        return `Name: ${this.name}, Age:${this.age}`;
+    },
+};
+console.log(person.getDetails());
+
+interface Employee extends Person {
+    employeeId: number;
+}
+const employee: Employee = {
+    employeeId: 101,
+    name: "jacky",
+    age: 22,
+    getDetails() {
+        return `Name: ${this.name}, Age:${this.age}, Employee ID: ${this.employeeId}`;
+    },
+};
+
+console.log(employee.getDetails());
+
+interface Manager extends Person, DogOwner {
+    employees: Employee[];
+    managePeople(): void;
+}
+let m1: Manager = {
+    employees: [employee],
+    name: "hari",
+    age: 34,
+    dogName: "sheru",
+    getDogDetails() {
+        return `Dog Name: ${this.dogName}`;
+    },
+    getDetails() {
+        return `Name: ${this.name}, Age:${this.age}`;
+    },
+    managePeople() {
+        console.log(`managing ${this.employees.length} people`);
+    },
+};
+console.log("here");
+
+console.log(m1);
+console.log(m1.getDetails());
+console.log(m1.getDogDetails());
+m1.managePeople();
