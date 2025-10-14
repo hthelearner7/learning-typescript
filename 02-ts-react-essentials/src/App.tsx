1c4eaa43b43a2eb337bf8a8dd766796e3805efc6
Himanshu Juyal <himanshujuyal280404@gmail.com>
2025-10-14 10:42:44 +0530

    02-props

--- 02-ts-react-essentials/src/App.tsx
+++ 02-ts-react-essentials/src/App.tsx
@@ -1,9 +1,14 @@
-import Component from "./starter/01-return";
+// import Component from "./starter/01-return";
+import Component from "./starter/02-props";
 
 function App() {
     return (
         <main>
-            <Component />
+            <Component name="peter" id={123}>
+                <h2>i am a heading passed as a child component</h2>
+            </Component>
+            <hr />
+            <Component name="jack" id={234} />
         </main>
     );
 }
--- 02-ts-react-essentials/src/starter/02-props/index.tsx
+++ 02-ts-react-essentials/src/starter/02-props/index.tsx
@@ -1,9 +1,39 @@
-function Component() {
-  return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Props</h2>
-    </div>
-  );
+import type { PropsWithChildren } from "react";
+
+// import type React from "react";
+// type ComponentProps = {
+//     name: string;
+//     id: number;
+//     children?: React.ReactNode;
+// };
+// console.log({ name, id });
+// function Component({ name, id }: { name: string; id: number }) { // 01-method
+// function Component({ name, id }: ComponentProps) {
+
+//     return (
+//         <div>
+//             <h2>React & Typescript</h2>
+//             <h2>Props</h2>
+//             <p>Name: {name}</p>
+//             <p>Id: {id}</p>
+//         </div>
+//     );
+// }
+
+type ComponentProps = PropsWithChildren<{
+    name: string;
+    id: number;
+}>;
+
+function Component(props: ComponentProps) {
+    return (
+        <div>
+            <h2>React & Typescript</h2>
+            <h2>Props</h2>
+            <p>Name: {props.name}</p>
+            <p>Id: {props.id}</p>
+            {props.children}
+        </div>
+    );
 }
 export default Component;
