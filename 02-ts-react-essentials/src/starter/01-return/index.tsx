7ff374de8530fc2c67dc6b7a0079e951d3b3c206
Himanshu Juyal <himanshujuyal280404@gmail.com>
2025-10-14 10:13:04 +0530

    01-component-return

--- 02-ts-react-essentials/src/starter/01-return/index.tsx
+++ 02-ts-react-essentials/src/starter/01-return/index.tsx
@@ -1,9 +1,14 @@
-function Component() {
-  return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Return Type</h2>
-    </div>
-  );
+import type { JSX } from "react";
+
+// function Component(): void {} // error
+
+function Component(): JSX.Element | null | string {
+    return (
+        <div>
+            <h2>Return Type</h2>
+        </div>
+    );
+    return null;
+    return "hello";
 }
 export default Component;
