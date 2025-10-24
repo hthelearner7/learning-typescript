5f59d6fc3d5a9b57a0af1dd90b5728e66fe64244
Himanshu Juyal <himanshujuyal280404@gmail.com>
2025-10-24 23:13:40 +0530

    07-reducers

--- 02-ts-react-essentials/src/App.tsx
+++ 02-ts-react-essentials/src/App.tsx
@@ -2,8 +2,9 @@
 // import Component from "./starter/02-props";
 // import Component from "./starter/03-state";
 // import Component from "./starter/05-challenge";
+// import Component from "./starter/06-context";
 
-import Component from "./starter/06-context";
+import Component from "./starter/07-reducers";
 
 function App() {
     return (
